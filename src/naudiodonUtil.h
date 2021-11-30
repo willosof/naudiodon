fb7dd9f593853504d4129e181a99d5712a41ccaa
Simon Rogers <scriptorian@streampunk.media>
2021-11-30 10:47:16 +0000

    Fix license error

--- src/naudiodonUtil.h
+++ src/naudiodonUtil.h
@@ -1,22 +1,16 @@
-/*
-  Aerostat Beam Coder - Node.js native bindings for FFmpeg.
-  Copyright (C) 2019  Streampunk Media Ltd.
+/* Copyright 2021 Streampunk Media Ltd.
 
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-  https://www.streampunk.media/ mailto:furnace@streampunk.media
-  14 Ormiscaig, Aultbea, Achnasheen, IV22 2JJ  U.K.
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
 */
 
 #ifndef BEAMCODER_UTIL_H
